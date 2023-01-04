6aafa7ec400916f43d73e1f6e140470a0c9c7c05
Thach Do <thach.do@smartcar.com>
2023-01-04 14:24:10 -0800

    installed dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -132,7 +132,7 @@ const IndexPage = () => {
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
+        Change <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time. 😎
       </p>
       <ul style={listStyles}>
