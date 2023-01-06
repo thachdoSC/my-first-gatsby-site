da9d0887dfebd987da149b87405458b09003015f
Thach Do <thach.do@smartcar.com>
2023-01-05 16:39:03 -0800

    rendered post data using mdx plugin

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,9 +8,13 @@ const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My cool posts will go in here</p>
-      <ul>
-        {data.allMdx.nodes.map(node => <li key={node.id}>{node.frontmatter.title}</li>)}
-      </ul>
+      {data.allMdx.nodes.map(node => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   )
 }
